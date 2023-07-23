218c3593a103e7eee255a3ec920c80315eee9091
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-07-23 12:29:40 +0200

    fix CI & linting

--- util.ts
+++ util.ts
@@ -1,7 +1,7 @@
 import * as crypto from 'crypto'
 const stringify = require('json-stringify-deterministic')
 
-export function generateHMAC(secret: string, properties: Record<string, any>): string {
+export function generateHMAC(secret: string, properties: Record<string, unknown>): string {
     const hmac = crypto.createHmac('sha256', secret)
     hmac.update(stringify(properties))
     return hmac.digest('hex')
